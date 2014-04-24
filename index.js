6e4cf4e480fd5a906b8a6567d3577236d2f81490
Fernando Derkoski <brilvio@gmail.com>
2014-04-24 10:04:29 -0300

    Add express,routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var Youtube = require("youtube-api");
-for (var i in Youtube) {
-    console.log(i);
-}
-var http = require("http");
+var express = require('express'),
+    videos = require('./routes/videos');
+ 
+var app = express();
+ 
+app.get('/videos', videos.listAllActivities);
+
 // You have to provide the credentials, first (in credentials.json file: rename .templ.json into json)
 var credentials = require("./credentials");
 var request = require("request");
@@ -11,100 +13,86 @@ credentials.scope = "https://www.googleapis.com/auth/youtube";
 credentials.response_type = "code";
 credentials.access_type = "offline";
 
-http.createServer(function (req, res) {
+app.get('/',function(req, res){
 
-    if (req.url === "/") {
-        if (typeof ACCESS_TOKEN !== "undefined") {
-            Youtube.authenticate({
-                type: "oauth",
-                token: ACCESS_TOKEN
-            });
+	var authUrl = "https://accounts.google.com/o/oauth2/auth?";
 
-            Youtube.activities.list({"part": "snippet,contentDetails", "home":true, "maxResults": 50}, function (err, data) {
-                var response = "";
-                response += "Error: " + JSON.stringify(err, null, 4) + "\n";
-                response += "Data: " + JSON.stringify(data, null, 4);
-                res.end(response);
-            });
+	for (var key in credentials) {
+		console.log(key, credentials[key]);
+		if (key === "client_secret") { continue; }
 
-            return;
-        }
+		authUrl += "&" + key + "=" + credentials[key];
+	}
+    if (typeof ACCESS_TOKEN == "undefined") {
+	  var html = "Click <a href='" + authUrl + "'>here</a> to get the access token.";
+	} else {	
+	  var html = "Click <a href='http://localhost:3000/videos'>here</a> to see the list of all activities.";
+	}
+	res.setHeader("Content-Type", "text/html");
+	res.end(html);
+	return;
+});
 
-        var authUrl = "https://accounts.google.com/o/oauth2/auth?";
+app.get('/oauth2callback',function(req, res) {
+	var url = req.url;
 
-        for (var key in credentials) {
-            console.log(key, credentials[key]);
-            if (key === "client_secret") { continue; }
+	if (url.indexOf("error") !== -1) {
+		return res.end("Error.");
+	}
 
-            authUrl += "&" + key + "=" + credentials[key];
-        }
+	if (url.indexOf("?code=") === -1) {
+		return res.end("Invalid request.");
+	}
 
-        var html = "Click <a href='" + authUrl + "'>here</a> to get the access token.";
-        res.setHeader("Content-Type", "text/html");
-        res.end(html);
-        return;
-    }
+	var code = url;
+	code = code.substring(code.indexOf("?code=") + 6);
 
-    if (req.url.indexOf("oauth2callback") !== -1) {
+	if (!code) {
+		return res.end("Code is missing.");
+	}
 
-        var url = req.url;
+	var formData = "code=" + code +
+				   "&client_id=" + credentials.client_id +
+				   "&client_secret=" + credentials.client_secret +
+				   "&redirect_uri=" + credentials.redirect_uri +
+				   "&grant_type=authorization_code";
 
-        if (url.indexOf("error") !== -1) {
-            return res.end("Error.");
-        }
+	var options = {
+		url: "https://accounts.google.com/o/oauth2/token",
+		headers: {'content-type' : 'application/x-www-form-urlencoded'},
+		method: "POST",
+		body: formData
+	};
 
-        if (url.indexOf("?code=") === -1) {
-            return res.end("Invalid request.");
-        }
+	request(options, function (err, response, body) {
 
-        var code = url;
-        code = code.substring(code.indexOf("?code=") + 6);
+		if (err) {
+			return res.end(err);
+		}
 
-        if (!code) {
-            return res.end("Code is missing.");
-        }
+		try {
+			body = JSON.parse(body);
+		} catch (e) {
+			return res.end(e.message + " :: " + body);
+		}
+		if (body.error) {
+			return res.end(err);
+		}
 
-        var formData = "code=" + code +
-                       "&client_id=" + credentials.client_id +
-                       "&client_secret=" + credentials.client_secret +
-                       "&redirect_uri=" + credentials.redirect_uri +
-                       "&grant_type=authorization_code";
+		// success
+		if (body.access_token) {
+			ACCESS_TOKEN = body.access_token;
+			var html = "Click <a href='http://localhost:3000'>here</a> to go back.</br> Access token: " + ACCESS_TOKEN;
+			res.setHeader("Content-Type", "text/html");
+	        return res.end(html);
+		}
 
-        var options = {
-            url: "https://accounts.google.com/o/oauth2/token",
-            headers: {'content-type' : 'application/x-www-form-urlencoded'},
-            method: "POST",
-            body: formData
-        };
+		return res.end("Something wrong: \n" + JSON.stringify(body, null, 4));
+	});
 
-        request(options, function (err, response, body) {
-
-            if (err) {
-                return res.end(err);
-            }
-
-            try {
-                body = JSON.parse(body);
-            } catch (e) {
-                return res.end(e.message + " :: " + body);
-            }
-            if (body.error) {
-                return res.end(err);
-            }
-
-            // success
-            if (body.access_token) {
-                ACCESS_TOKEN = body.access_token;
-                return res.end("Access token: " + ACCESS_TOKEN);
-            }
-
-            return res.end("Something wrong: \n" + JSON.stringify(body, null, 4));
-        });
-
-        return;
-    }
-
-    res.end("404 - Not found.");
-}).listen(3000);
+	return;    
+});
+ 
+app.listen(3000);
 
 console.log("Open: http://localhost:3000");
